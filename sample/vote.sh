ee85f57c532f370e8cd276119ccfcf7cac5aa204
Renjith RP <93483303+Renjith20@users.noreply.github.com>
2022-04-07 18:31:06 +0530

    sample prgrm

--- sample/vote.sh
+++ sample/vote.sh
@@ -3,8 +3,8 @@ echo "Enter your age:"
 read a
 if [ $a -lt 18 ]
 then
-	echo "Not Eligible for voting"
+	echo  "Not Eligible for voting"
 else
-        echo "Eligible for voting"
+        echo  "Eligible for voting"
 fi
 
