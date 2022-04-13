6550958979b4f6f79e323865549c287a2cd03ea0
Renjith RP <93483303+Renjith20@users.noreply.github.com>
2022-04-13 15:21:04 +0530

    Add files via upload

---
+++ areofcircle.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+function area()
+{
+	echo Entered Radius: $1
+	PI=3.14159
+	a=$(echo "$PI*($1^2)"|bc)
+        echo Area= $a
+}
+
+echo "Enter the radius"
+read r
+area $r
---
+++ functioneg.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+function print()
+{
+	echo "Hello $1"
+}
+print Angadev
+print Prejin
